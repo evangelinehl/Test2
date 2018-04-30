b4103f4d3d6e64048ff6869655668e133218ab98
Evangeline  Liu <evangelineliu@Evangelines-MBP.wv.cc.cmu.edu>
2018-04-30 19:26:02 -0400

    comment

--- src/test2/PrintHiThere.java
+++ src/test2/PrintHiThere.java
@@ -2,6 +2,10 @@ package test2;
 
 public class PrintHiThere {
 
+	/**
+	 * Main method
+	 * @param args no args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hi there!");
 		System.out.println("Let's print a rectangle!");
