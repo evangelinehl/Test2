106105369621ca5140dae59fd1778378043d96c1
Evangeline  Liu <evangelineliu@Evangelines-MBP.wv.cc.cmu.edu>
2018-04-30 18:24:53 -0400

    another commit

--- src/test2/PrintHiThere.java
+++ src/test2/PrintHiThere.java
@@ -4,7 +4,8 @@ public class PrintHiThere {
 
 	public static void main(String[] args) {
 		System.out.println("Hi there!");
-
+		System.out.println("Let's print a rectangle!");
+		PrintShapes.PrintRectangle(5, 7);
 	}
 
 }
