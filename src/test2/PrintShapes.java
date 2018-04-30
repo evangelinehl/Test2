bfa26d6423a657b4c6cd12d0abfca50538a784f2
Evangeline  Liu <evangelineliu@Evangelines-MBP.wv.cc.cmu.edu>
2018-04-30 18:24:11 -0400

    added comments

--- src/test2/PrintShapes.java
+++ src/test2/PrintShapes.java
@@ -1,6 +1,11 @@
 package test2;
 
 public class PrintShapes {
+	/**
+	 * Prints a rectangled w/ specified parameters
+	 * @param width Width
+	 * @param height Height
+	 */
 	public static void PrintRectangle(int width, int height) {
 		for(int i = 0; i < width; i++) {
 			System.out.print("^");
