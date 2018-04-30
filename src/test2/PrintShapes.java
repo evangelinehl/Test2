51f1ac7cd534cfb5a4d1187690537038077bbe42
Evangeline  Liu <evangelineliu@Evangelines-MBP.wv.cc.cmu.edu>
2018-04-30 18:23:39 -0400

    another commit

--- src/test2/PrintShapes.java
+++ src/test2/PrintShapes.java
@@ -5,11 +5,17 @@ public class PrintShapes {
 		for(int i = 0; i < width; i++) {
 			System.out.print("^");
 		}
+		System.out.println();
 		for(int i = 0; i < height - 2; i++) {
+			System.out.print("^");
+			for(int j = 0; j < width - 2; j++) {
+				System.out.print(" ");
+			}
 			System.out.println("^");
 		}
 		for(int i = 0; i < width; i++) {
 			System.out.print("^");
 		}
+		System.out.println();
 	}
 }
